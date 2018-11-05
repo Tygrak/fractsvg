cbfe1ab894b75168da0c8b1ec9788928e7bb1361
Tygrak <23155456+Tygrak@users.noreply.github.com>
2018-11-05 15:35:43 +0100

    Added new fractal generators

---
+++ svghelper.py
@@ -0,0 +1,36 @@
+import math
+
+class SVGImage:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.objects = []
+
+    def addObject(self, obj):
+        self.objects.append(obj)
+
+    def generateImageString(self):
+        imageString = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="{0}" height="{1}">\n'.format(self.width, self.height)
+        for obj in self.objects:
+            imageString += '  {0}\n'.format(obj.generateObjectString())
+        imageString += '</svg>\n'
+        return imageString
+
+class SVGPolygon:
+    def __init__(self, points = [], strokeWidth = 4):
+        self.points = points
+        self.stroke = "black"
+        self.strokeWidth = strokeWidth
+        self.fill = "none"
+
+    def addPoint(self, x, y):
+        self.points.append([x, y])
+
+    def generateObjectString(self):
+        pointsString = ""
+        for point in self.points:
+            pointsString += "{0},{1} ".format(point[0], point[1])
+        pointsString = pointsString.rstrip()
+        styleString = 'stroke="{0}" stroke-width="{1}" fill="{2}"'.format(self.stroke, self.strokeWidth, self.fill)
+        objectString = '<polygon points="{0}" {1} />'.format(pointsString, styleString)
+        return objectString
--- test.py
+++ test.py
@@ -1,39 +1,5 @@
 import math
-
-class SVGImage:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.objects = []
-
-    def addObject(self, obj):
-        self.objects.append(obj)
-
-    def generateImageString(self):
-        imageString = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="{0}" height="{1}">\n'.format(self.width, self.height)
-        for obj in self.objects:
-            imageString += '  {0}\n'.format(obj.generateObjectString())
-        imageString += '</svg>\n'
-        return imageString
-
-class SVGPolygon:
-    def __init__(self, points = []):
-        self.points = points
-        self.stroke = "black"
-        self.strokeWidth = 4
-        self.fill = "none"
-
-    def addPoint(self, x, y):
-        self.points.append([x, y])
-
-    def generateObjectString(self):
-        pointsString = ""
-        for point in self.points:
-            pointsString += "{0},{1} ".format(point[0], point[1])
-        pointsString = pointsString.rstrip()
-        styleString = 'stroke="{0}" stroke-width="{1}" fill="{2}"'.format(self.stroke, self.strokeWidth, self.fill)
-        objectString = '<polygon points="{0}" {1} />'.format(pointsString, styleString)
-        return objectString
+from svghelper import *
 
 def getHalfwayPoint(a, b):
     x = (b[0]-a[0])/2 + a[0]
@@ -51,33 +17,152 @@ def generateSierpenski(iterations, polygons = [SVGPolygon([[50, 450], [450, 450]
         newPolygon = SVGPolygon(points)
         newPolygon.strokeWidth = polygons[i].strokeWidth*7/9
         polygons.append(newPolygon)
-        oldPoints = polygons[i].points
         points = []
         points.append(getHalfwayPoint(oldPoints[1], oldPoints[0]))
         points.append((oldPoints[1][0], oldPoints[1][1]))
         points.append(getHalfwayPoint(oldPoints[1], oldPoints[2]))
-        newPolygon = SVGPolygon(points)
-        newPolygon.strokeWidth = polygons[i].strokeWidth*7/9
+        newPolygon = SVGPolygon(points, polygons[i].strokeWidth*7/9)
         polygons.append(newPolygon)
         points = []
         points.append(getHalfwayPoint(oldPoints[2], oldPoints[0]))
         points.append(getHalfwayPoint(oldPoints[2], oldPoints[1]))
         points.append((oldPoints[2][0], oldPoints[2][1]))
-        newPolygon = SVGPolygon(points)
-        newPolygon.strokeWidth = polygons[i].strokeWidth*7/9
+        newPolygon = SVGPolygon(points, polygons[i].strokeWidth*7/9)
         polygons.append(newPolygon)
         polygons.pop(i)
     if iterations > 0:
         polygons = generateSierpenski(iterations-1, polygons)
     return polygons
 
+def generateAltKoch(iterations, polygons = [SVGPolygon([[100, 500], [400, 500]])]):
+    for i in range(len(polygons)-1, -1, -1):
+        oldPoints = polygons[i].points
+        #print(i, len(polygons), oldPoints)
+        points = []
+        x = (oldPoints[1][0]-oldPoints[0][0])/2 + oldPoints[0][0]
+        y = (oldPoints[1][1]-oldPoints[0][1])/2 + oldPoints[0][1]
+        vectx = (oldPoints[1][0]-oldPoints[0][0])/2.265
+        vecty = (oldPoints[1][1]-oldPoints[0][1])/2.265
+        secondPoint = (x+vecty, y-vectx)
+        points.append(oldPoints[0])
+        points.append(secondPoint)
+        newPolygon = SVGPolygon(points, polygons[i].strokeWidth*7.75/9)
+        newPolygon.stroke = polygons[i].stroke
+        polygons.append(newPolygon)
+        points = []
+        points.append(secondPoint)
+        points.append(oldPoints[1])
+        newPolygon = SVGPolygon(points, polygons[i].strokeWidth*7.75/9)
+        newPolygon.stroke = polygons[i].stroke
+        polygons.append(newPolygon)
+        #polygons.pop(i)
+    if iterations > 0:
+        polygons = generateAltKoch(iterations-1, polygons)
+    return polygons
+
+def generateKoch(iterations, polygons = [SVGPolygon([[100, 500], [400, 500]])]):
+    for i in range(len(polygons)-1, -1, -1):
+        oldPoints = polygons[i].points
+        #print(i, len(polygons), oldPoints)
+        points = []
+        x = (oldPoints[1][0]-oldPoints[0][0])/2 + oldPoints[0][0]
+        y = (oldPoints[1][1]-oldPoints[0][1])/2 + oldPoints[0][1]
+        vectx = (oldPoints[1][0]-oldPoints[0][0])/2.75
+        vecty = (oldPoints[1][1]-oldPoints[0][1])/2.75
+        halfPoint = (x+vecty, y-vectx)
+        x = (oldPoints[1][0]-oldPoints[0][0])/3 + oldPoints[0][0]
+        y = (oldPoints[1][1]-oldPoints[0][1])/3 + oldPoints[0][1]
+        onethirdPoint = (x, y)
+        x = 2*(oldPoints[1][0]-oldPoints[0][0])/3 + oldPoints[0][0]
+        y = 2*(oldPoints[1][1]-oldPoints[0][1])/3 + oldPoints[0][1]
+        twothirdPoint = (x, y)
+        points.append(oldPoints[0])
+        points.append(onethirdPoint)
+        newPolygon = SVGPolygon(points, polygons[i].strokeWidth*7.55/9)
+        polygons.append(newPolygon)
+        points = []
+        points.append(onethirdPoint)
+        points.append(halfPoint)
+        newPolygon = SVGPolygon(points, polygons[i].strokeWidth*7.55/9)
+        polygons.append(newPolygon)
+        points = []
+        points.append(halfPoint)
+        points.append(twothirdPoint)
+        newPolygon = SVGPolygon(points, polygons[i].strokeWidth*7.55/9)
+        polygons.append(newPolygon)
+        points = []
+        points.append(twothirdPoint)
+        points.append(oldPoints[1])
+        newPolygon = SVGPolygon(points, polygons[i].strokeWidth*7.55/9)
+        polygons.append(newPolygon)
+        points = []
+        polygons.pop(i)
+    if iterations > 0:
+        polygons = generateKoch(iterations-1, polygons)
+    return polygons
+
+
+def generateDragon(iterations, polygons = [SVGPolygon([[125, 300], [425, 300]])]):
+    for i in range(len(polygons)-1, -1, -1):
+        oldPoints = polygons[i].points
+        points = []
+        x = (oldPoints[1][0]-oldPoints[0][0])/2 + oldPoints[0][0]
+        y = (oldPoints[1][1]-oldPoints[0][1])/2 + oldPoints[0][1]
+        vectx = (oldPoints[1][0]-oldPoints[0][0])/2
+        vecty = (oldPoints[1][1]-oldPoints[0][1])/2
+        secondPoint = (x+vecty, y-vectx)
+        points.append(oldPoints[0])
+        points.append(secondPoint)
+        newPolygon = SVGPolygon(points, polygons[i].strokeWidth*7.85/9)
+        newPolygon.stroke = polygons[i].stroke
+        polygons.append(newPolygon)
+        points = []
+        points.append(oldPoints[1])
+        points.append(secondPoint)
+        newPolygon = SVGPolygon(points, polygons[i].strokeWidth*7.85/9)
+        newPolygon.stroke = polygons[i].stroke
+        polygons.append(newPolygon)
+        polygons.pop(i)
+    if iterations > 0:
+        polygons = generateDragon(iterations-1, polygons)
+    return polygons
+
 image = SVGImage(500, 500)
-for polygon in generateSierpenski(7, [SVGPolygon([[50, 450], [450, 450], [250, 250]]), SVGPolygon([[450, 450], [450, 50], [250, 250]]),
-     SVGPolygon([[450, 50], [50, 50], [250, 250]]), SVGPolygon([[50, 50], [50, 450], [250, 250]])]):
-    image.addObject(polygon)
+bgSquare = SVGPolygon([[0, 0], [500, 0], [500, 500], [0, 500]], 0)
+bgSquare.fill = "white"
+image.addObject(bgSquare)
+
+#for polygon in generateSierpenski(7, [SVGPolygon([[50, 450], [450, 450], [250, 250]]), SVGPolygon([[450, 450], [450, 50], [250, 250]]),
+#     SVGPolygon([[450, 50], [50, 50], [250, 250]]), SVGPolygon([[50, 50], [50, 450], [250, 250]])]):
+#    image.addObject(polygon)
 #for polygon in generateSierpenski(6):
 #    image.addObject(polygon)
+polyg1 = SVGPolygon([[400, 250], [100, 250]])
+polyg1.stroke = "rgb(255, 0, 191)"
+polyg2 = SVGPolygon([[100, 250], [400, 250]])
+polyg2.stroke = "rgb(0, 64, 255)"
+polyg3 = SVGPolygon([[250, 400], [250, 100]])
+polyg3.stroke = "rgb(0, 255, 64)"
+polyg4 = SVGPolygon([[250, 100], [250, 400]])
+polyg4.stroke = "rgb(255, 191, 0)"
+for polygon in generateAltKoch(7, [polyg1, polyg2, polyg3, polyg4]):
+    image.addObject(polygon)
+#for polygon in generateKoch(5, [SVGPolygon([[500, 250], [0, 250]]), SVGPolygon([[0, 250], [500, 250]])]):
+#    image.addObject(polygon)
+#for polygon in generateKoch(6, [SVGPolygon([[150, 250+100*(3**0.5)], [350, 250+100*(3**0.5)]]), SVGPolygon([[350, 250+100*(3**0.5)], [450, 250]]),
+#                            SVGPolygon([[450, 250], [350, 250-100*(3**0.5)]]), SVGPolygon([[350, 250-100*(3**0.5)], [150, 250-100*(3**0.5)]]),
+#                            SVGPolygon([[150, 250-100*(3**0.5)], [50, 250]]), SVGPolygon([[50, 250], [150, 250+100*(3**0.5)]])]):
+#    image.addObject(polygon)
+#for polygon in generateDragon(13, [SVGPolygon([[130, 250], [370, 250]])]):
+#    image.addObject(polygon)
+#dragon1 = SVGPolygon([[130, 250], [370, 250]])
+#dragon1.stroke = "red"
+#dragon1.strokeWidth = 4.5
+#dragon2 = SVGPolygon([[370, 250], [130, 250]])
+#dragon2.stroke = "blue"
+#dragon2.strokeWidth = 4.5
+#for polygon in generateDragon(13, [dragon1, dragon2]):
+#    image.addObject(polygon)
 
-with open("output.svg", "w") as svgFile:
+with open("output9.svg", "w") as svgFile:
     svgFile.write(image.generateImageString())
-
